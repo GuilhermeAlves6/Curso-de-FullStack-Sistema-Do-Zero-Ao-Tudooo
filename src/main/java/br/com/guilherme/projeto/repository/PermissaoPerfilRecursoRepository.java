d052b39fa4829aa8315b275d9bc3c242bf0b0ae1
Guilherme Alves <Guilhermealves53139@gmail.com>
2024-07-19 23:04:37 -0300

    CRUD DO OBJETO Permissao Perfil Recurso backend + data.sql com DADOS

---
+++ src/main/java/br/com/guilherme/projeto/repository/PermissaoPerfilRecursoRepository.java
@@ -0,0 +1,9 @@
+package br.com.guilherme.projeto.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.guilherme.projeto.entity.PermissaoPerfilRecursoEntity;
+
+public interface PermissaoPerfilRecursoRepository extends JpaRepository<PermissaoPerfilRecursoEntity, Long>{
+
+}
