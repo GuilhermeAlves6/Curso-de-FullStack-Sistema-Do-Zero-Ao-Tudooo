4a37d069c4417fbe8c6c89aaa33d96bfc2dda5a8
Guilherme Alves <Guilhermealves53139@gmail.com>
2024-07-22 22:49:34 -0300

    ADICIONANDO SRPING SECURITY E JWT NO PROJETO PARTE 3

--- src/main/java/br/com/guilherme/projeto/security/jwt/JwtUtils.java
+++ src/main/java/br/com/guilherme/projeto/security/jwt/JwtUtils.java
@@ -1,5 +1,6 @@
 package br.com.guilherme.projeto.security.jwt;
 
+import java.nio.charset.MalformedInputException;
 import java.security.Key;
 import java.util.Date;
 
@@ -9,8 +10,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import br.com.guilherme.projeto.service.UserDetailsImpl;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
@@ -24,22 +28,31 @@ public class JwtUtils {
 	private int jwtExpirationMs;
 
 	public String generateTokenFromUserDetailsImpl(UserDetailsImpl userDetail) {
-		return Jwts.builder().setSubject(userDetail.getUsername()
-				.setIssuedAt(new Date())
+		return Jwts.builder().setSubject(userDetail.getUsername()).setIssuedAt(new Date())
 				.setExpiration(new Date(new Date().getTime() + jwtExpirationMs))
 				.signWith(getSigninKey(), SignatureAlgorithm.HS512).compact();
 	}
-	
+
 	public Key getSigninKey() {
 		SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
-		return Key ;
+		return key;
 	}
-	
+
 	public boolean validateJwtToken(String authToken) {
-		
+		try {
+			Jwts.parser().setSigningKey(getSigninKey()).build().parseClaimsJws(authToken);
+			return true;
+
+		} catch (MalformedJwtException e) {
+			System.out.println("Token inválido" + e.getMessage());
+		} catch (ExpiredJwtException e) {
+			System.out.println("Token inválido" + e.getMessage());
+		} catch (UnsupportedJwtException e) {
+			System.out.println("Token inválido" + e.getMessage());
+		} catch (IllegalArgumentException e) {
+			System.out.println("Token Argumento inválido" + e.getMessage());
+		}
+		return false;
+
 	}
-	
 }
-		
-				
-		
