9fde789f8498b95895290411ad48e7689e888f94
Guilherme Alves <Guilhermealves53139@gmail.com>
2024-07-23 23:57:52 -0300

    ADICIONANDO SRPING SECURITY E JWT NO PROJETO PARTE 4

--- src/main/java/br/com/guilherme/projeto/security/WebSecurityConfig.java
+++ src/main/java/br/com/guilherme/projeto/security/WebSecurityConfig.java
@@ -1,10 +1,32 @@
 package br.com.guilherme.projeto.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import br.com.guilherme.projeto.security.jwt.AuthEntryPointJwt;
 
 @Configuration
 @EnableMethodSecurity
 public class WebSecurityConfig {
+	
+	@Autowired
+	private AuthEntryPointJwt unauthorizedHandler;
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public AuthenticationManager authenticationManger(AuthenticationConfiguration authentionConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	
+	
 
 }
