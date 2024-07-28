5568acbc781cd3ef71e1a8bf8789b58844327137
Guilherme Alves <Guilhermealves53139@gmail.com>
2024-07-27 23:29:25 -0300

    ADICIONANDO SRPING SECURITY E JWT NO PROJETO PARTE 8

--- src/main/java/br/com/guilherme/projeto/service/UserDetailsImpl.java
+++ src/main/java/br/com/guilherme/projeto/service/UserDetailsImpl.java
@@ -1,12 +1,9 @@
 package br.com.guilherme.projeto.service;
 
-import java.util.ArrayList;
 import java.util.Collection;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
-import br.com.guilherme.projeto.entity.UsuarioEntity;
 
 public class UserDetailsImpl implements UserDetails {
 	
@@ -14,33 +11,11 @@ public class UserDetailsImpl implements UserDetails {
 	
 	private String name;
 	
-	private String username;
+	private String Username;
 	
 	private String email;
 	
 	private String password;
-	
-	
-	
-	public UserDetailsImpl(Long id, String name, String username, String email,
-			Collection<? extends GrantedAuthority> authorities) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.username = username;
-		this.email = email;
-		this.authorities = authorities;
-	}
-
-	public static UserDetailsImpl build(UsuarioEntity usuario) {
-		return new UserDetailsImpl(
-				usuario.getId(),
-				usuario.getNome(), 
-				usuario.getLogin(), 
-				usuario.getEmail(), 
-				new ArrayList<>());
-		
-	}
 	
 	private Collection<? extends GrantedAuthority> authorities;
 
@@ -58,9 +33,9 @@ public class UserDetailsImpl implements UserDetails {
 	public String getUsername() {
 		return username;
 	}
-	
+
 	@Override
-	public boolean isAccountNonExpired () {
+	public boolean isAccountNonExpired() {
 		return true;
 	}
 	
@@ -68,19 +43,15 @@ public class UserDetailsImpl implements UserDetails {
 	public boolean isAccountNonLocked() {
 		return true;
 	}
+	
 	@Override
-	public boolean isCredentialsNonExpired () {
+	public boolean isCredentialsNonExpired() {
 		return true;
 	}
 	
 	@Override
-	public boolean isEnabled () {
+	public boolean isEnabled() {
 		return true;
 	}
-	
+
 }
-
-	
-
-	
-	
